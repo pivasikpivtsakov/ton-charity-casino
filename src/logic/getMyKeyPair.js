6086f2909a45304559ae098f3bfb4a9581921550
pivasikpivtsakov <pivasikpivtsakov@github.com>
2022-07-02 16:12:56 +0500

    getMyKeyPair.js

---
+++ src/logic/getMyKeyPair.js
@@ -0,0 +1,13 @@
+import {isPasswordNeeded, mnemonicToKeyPair, validateMnemonic} from 'tonweb-mnemonic';
+
+const getMyKeyPair = async (mnemonic, password) => {
+    if (await isPasswordNeeded(mnemonic)) {
+        if (!password) {
+            throw {passwordNeeded: true};
+        }
+    }
+    if (!await validateMnemonic(mnemonic, password)){
+        throw {invalidMnemonic: true};
+    }
+    return await mnemonicToKeyPair(mnemonic, password);
+}
