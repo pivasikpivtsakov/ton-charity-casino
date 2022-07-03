723fd4e8d55526e09cd25b0bf365fb42b6d3f02d
PC\Negor <alienlord9280@gmail.com>
2022-07-03 17:10:01 +0500

    естко надристал

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-const webpack = require("webpack");
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-      })
-    ]
-  },
-})
+});
