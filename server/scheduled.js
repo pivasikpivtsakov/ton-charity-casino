f4d8ad225cceeb4f2e39c5797cf86abcc9adc49a
nikololiahim <olomishcak@outlook.com>
2022-07-03 13:50:55 +0300

    whatever

--- server/scheduled.js
+++ server/scheduled.js
@@ -1,5 +1,6 @@
 const CronJob = require("node-cron");
 
+
 exports.runEveryMinute = (action) => {
   const scheduledJobFunction = CronJob.schedule("* * * * *", () => {
     console.log("Executing action");
