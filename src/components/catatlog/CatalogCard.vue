a61fe9865b8e00dc757049065dc1b1e354378d0d
pivasikpivtsakov <pivasikpivtsakov@github.com>
2022-07-03 20:24:41 +0500

    file fix

--- src/components/catatlog/CatalogCard.vue
+++ src/components/catatlog/CatalogCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col	shadow-black w-full items-center border-2 border-gray-600 rounded-md py-4">
-    <img :src="require(`../../assets/beer/beer${index+1}.jpg`)" alt="beerpic" class="beer_ava cursor-pointer mb-4"/>
+    <img :src="require(`../../assets/beer/${index+1}.jpg`)" alt="beerpic" class="beer_ava cursor-pointer mb-4"/>
     <h2 class="mb-4 cursor-pointer">Beer {{ index }}</h2>
     <div class="flex justify-between px-4 w-full">
         <span class="text-sm font-semibold text-slate-500">{{beer.country}}</span>
